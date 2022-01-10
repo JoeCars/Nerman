54e8850b6f8b1ca9285c3a2bb76a7e6651d2c16b
JoeCars <help@joelcares.net>
2022-01-09 22:05:13 -0600

    trying out fetch

--- helpers/twitter.js
+++ helpers/twitter.js
@@ -57,9 +57,7 @@ async function post(content, media_urls) {
 
 async function getBase64ImageFromURLFetch(url) {
 
-  console.log(url);
-
-  const response = await fetch('url');
+  const response = await fetch(url);
   const data = await response.json();
 
   console.log(data);
