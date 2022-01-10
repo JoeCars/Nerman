842ee796624a30366d2a2bdf7643f0ae88cd04d0
JoeCars <help@joelcares.net>
2022-01-09 20:33:36 -0600

    redidding the do

--- helpers/twitter.js
+++ helpers/twitter.js
@@ -82,7 +82,7 @@ async function getImageFromUrl(url) {
 
 async function uploadImageToTwitter(media_data, content) {
 
-  await T.post('media/upload', { "media_data": media_data }, function (err, data, response) {
+  await T.post('media/upload', { "media_data": media_data }, async function (err, data, response) {
 
     let mediaIdStr = data.media_id_string
     let altText = content;
