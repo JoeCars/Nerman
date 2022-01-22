66d934cb0c63cb7165c5d414c9ccb56a3bd6ad07
JulesRuby <JulesRuby@users.noreply.github.com>
2022-01-22 13:14:17 -0700

    Added a log to the deploy command script

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,17 +5,20 @@ const clientId = process.env.DISCORD_CLIENT_ID;
 const guildId = process.env.DISCORD_GUILD_ID;
 const token = process.env.DISCORD_TOKEN;
 
-
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs
+	.readdirSync('./commands')
+	.filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-   const command = require(`./commands/${file}`);
-   commands.push(command.data.toJSON());
+	const command = require(`./commands/${file}`);
+	console.log('desploy command:', command);
+	commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-   .then(() => console.log('Successfully registered application commands.'))
-   .catch(console.error);
+rest
+	.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
