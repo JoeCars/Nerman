3183eb7cbc4a593e503d6a67515814342cd4bc6f
JulesRuby <JulesRuby@users.noreply.github.com>
2022-02-04 19:16:09 -0700

    added some error handling to /noun command

--- commands/noun.js
+++ commands/noun.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Message, MessageAttachment, MessageEmbed } = require('discord.js');
+const fetch = require('node-fetch');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,14 +12,34 @@ module.exports = {
 	async execute(interaction) {
 		const nounNum = interaction.options.getInteger('int');
 
+		const msgAttach = new MessageAttachment(); // will instantiate this here, to add a 500 error code noun image later if there is a bad response
+
 		//Opensea Link, Owner, previous auction info. Integrate Open Sea API
 
-		const msgAttach = new MessageAttachment(`https://noun.pics/${nounNum}.png`);
+		try {
+			const resp = await fetch(`https://noun.pics/${nounNum}.png`);
 
-		await interaction.reply({
-			content: `Noun ${nounNum}`,
-			files: [msgAttach],
-			ephemeral: true,
-		});
+			if (resp.status !== 200) {
+				await interaction.reply({
+					content: 'Unable to return Noun, are you sure this Noun exists yet?',
+					files: null,
+					ephemeral: true,
+				});
+				throw new Error(
+					'Unable to return Noun, are you sure this Noun exists yet?'
+				);
+			}
+
+			msgAttach.attachment = `https://noun.pics/${nounNum}.png`;
+
+			await interaction.reply({
+				content: `Noun ${nounNum}`,
+				files: [msgAttach],
+				ephemeral: true,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+
 	},
 };
