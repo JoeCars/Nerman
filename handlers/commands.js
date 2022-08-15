37a85384af19bcc3d7298d63b1544f392519a5c3
JulesRuby <JulesRuby@users.noreply.github.com>
2022-08-15 13:54:01 -0600

    Remove deploy-commands file (not neeeded, handled under handlers/commands.js now)

--- handlers/commands.js
+++ handlers/commands.js
@@ -1,4 +1,3 @@
-// const fs = require('fs');
 const { Collection } = require('discord.js');
 const { getFiles } = require('../utils/functions');
 const { REST } = require('@discordjs/rest');
@@ -11,24 +10,16 @@ const token = process.env.DISCORD_TOKEN;
 
 module.exports = async client => {
    const commandsArr = [];
+
    client.commands = new Collection();
-   // const commandFiles = fs
-   //    .readdirSync('./commands')
-   //    .filter(file => file.endsWith('.js'));
 
    const commands = getFiles('./commands', '.js');
 
    if (commands.length === 0) throw 'No slash commands provided';
 
-   // for (const file of commandFiles) {
-   //    const command = require(`./commands/${file}`);
-   //    commands.push(command.data.toJSON());
-   // }
-
    commands.forEach(commandFile => {
       const command = require(`../commands/${commandFile}`);
-      // console.log(command.data);
-      // console.log(command.data.name);
+
       if (command.data.name && typeof command.data.name === 'string') {
          commandsArr.push(command.data.toJSON());
          client.commands.set(command.data.name, command);
@@ -44,11 +35,6 @@ module.exports = async client => {
    });
 
    const rest = new REST({ version: '9' }).setToken(token);
-
-   // rest
-   // 	.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-   // 	.then(() => console.log('Successfully registered application commands.'))
-   // 	.catch(console.error);
 
    (async () => {
       try {
