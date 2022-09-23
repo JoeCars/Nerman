0e5d4e3430dabc34920b7a03b7ac4a85a9286dc7
JulesRuby <JulesRuby@users.noreply.github.com>
2022-09-22 22:23:33 -0600

    Modified this to use the newer library of stateofnouns

--- commands/emit-test-proposal.js
+++ commands/emit-test-proposal.js
@@ -1,15 +1,26 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const testProposal = require('../scratchcode/testGraphSample.json');
+const PollChannel = require('../db/schemas/PollChannel');
 const { log: l, time: t, timeEnd: te } = console;
+
+const propChannelId =
+   process.env.DEPLOY_STAGE === 'staging'
+      ? process.env.TESTNERMAN_NOUNCIL_CHAN_ID
+      : process.env.DEVNERMAN_NOUNCIL_CHAN_ID;
 
 module.exports = {
    data: new SlashCommandBuilder()
       .setName('emit-test-proposal')
-      .setDescription("Mimic Joel's event emitter for a new proposal and using some old data from a prior Nouns prop."),
+      .setDescription(
+         "Mimic Joel's event emitter for a new proposal and using some old data from a prior Nouns prop."
+      ),
 
    async execute(interaction) {
       const {
          client,
+         guild: {
+            channels: { cache: channelCache },
+         },
          member: {
             roles: { cache: roleCache },
          },
@@ -19,6 +30,18 @@ module.exports = {
 
       if (!roleCache.has('919784986641575946')) return;
 
-      client.emit('newProposal', interaction, testProposal);
+      const propChannel = await channelCache.get(propChannelId);
+
+      const channelConfig = await PollChannel.findOne(
+         {
+            channelId: propChannelId,
+         },
+         '_id allowedRoles quorum duration'
+      ).exec();
+
+      let message = await propChannel.send({content: 'Generating proposal poll...'});
+
+      // client.emit('newProposal', interaction, testProposal);
+      client.emit('newProposal', message, testProposal);
    },
 };
