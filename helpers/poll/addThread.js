6ee6e84a22f6915b37b3aafaf0c95877e487f4ca
JulesRuby <JulesRuby@users.noreply.github.com>
2022-10-12 12:18:33 -0600

    Created a helper to automatically add a thread to each poll created

---
+++ helpers/poll/addThread.js
@@ -0,0 +1,8 @@
+const { MessageEmbed, MessageButton, MessageActionRow } = require('discord.js');
+const { roleMention } = require('@discordjs/builders');
+
+const addThread = async data => {
+
+};
+
+module.exports = { addThread };
