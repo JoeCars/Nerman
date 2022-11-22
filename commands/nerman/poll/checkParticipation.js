cf82418cbfe01e0fc739bbd780d38bd9ea5f1765
JulesRuby <JulesRuby@users.noreply.github.com>
2022-11-21 17:08:38 -0700

    Altered the output of the user participation command to look neater

--- commands/nerman/poll/checkParticipation.js
+++ commands/nerman/poll/checkParticipation.js
@@ -1,10 +1,12 @@
 const { CommandInteraction } = require('discord.js');
+const { codeBlock } = require('@discordjs/builders');
+
 const { Types } = require('mongoose');
 const User = require('../../../db/schemas/User');
 const Poll = require('../../../db/schemas/Poll');
 const PollChannel = require('../../../db/schemas/PollChannel');
+
 const { log: l } = console;
-// const { lc } = require('../../../utils/functions');
 module.exports = {
    subCommand: 'nerman.participation',
    /**
@@ -14,32 +16,38 @@ module.exports = {
    async execute(interaction) {
       const {
          channelId,
-         channel: { name: channelName },
+         channel: {
+            name: channelName,
+            members: { cache: cmCache },
+         },
          guild: {
             members: { cache: mCache },
          },
-         // user: { username, nickname, discriminator },
       } = interaction;
 
-      // const config = await PollChannel.countDocuments({ channelId: channelId });
-      l({ interaction });
-      l({ channelName });
-      // console.log({ config });
       const configExists = await PollChannel.configExists(channelId);
 
       if (!configExists) {
-         // throw new Error('There are no configurations registered to this channel. You may only register from a channel in which polling has been configured.');
-         return interaction.reply({
-            content:
-               'There are no configurations registered to this channel. You may only register from a channel in which polling has been configured.',
-            ephemeral: true,
-         });
+         throw new Error('There are no configurations registered to this channel. You may only register from a channel in which polling has been configured.');
+         // return interaction.reply({
+         //    content:
+         //       'There are no configurations registered to this channel. You may only register from a channel in which polling has been configured.',
+         //    ephemeral: true,
+         // });
       }
 
-      // l({ mCache });
 
       const voterId = interaction.options.getString('discord-id');
+      const {
+         nickname,
+         user: { username, discriminator },
+      } = mCache.get(voterId);
+
       const voterDoc = await User.findOne().byDiscordId(voterId).exec();
+      const { eligiblePolls: witnessed, participatedPolls: participated } =
+         voterDoc.eligibleChannels.get(channelId);
+
+      l({ witnessed, participated });
 
       const {
          roles: { cache: memberRoles },
@@ -58,113 +66,44 @@ module.exports = {
             );
 
          const eligibleChannels = await User.findEligibleChannels(memberRoles);
+         l( { eligibleChannels });
 
-         //disabled
-         // const eligibleChannels = await PollChannel.find({
-         //    allowedRoles: { $in: [...memberRoles.keys()] },
-         // });
-
-         l('CHUNGA', { eligibleChannels });
-
-         // const channelMap = new Map();
-
-         // for (const channel of eligibleChannels) {
-         //    const polls = await Poll.aggregate([
-         //       {
-         //          $match: {
-         //             [`config`]: channel._id,
-         //             [`allowedUsers.${voterId}`]: { $exists: true },
-         //          },
-         //       },
-         //    ]).exec();
-
-         //    const statsObject = {
-         //       eligiblePolls: polls.length,
-         //       participatedPolls: polls.filter(
-         //          ({ allowedUsers }) => allowedUsers[voterId] === true
-         //       ).length,
-         //    };
-
-         //    l({ statsObject });
-
-         //    channelMap.set(channel.channelId, statsObject);
-         //    // const polls = await Poll.find({
-         //    //    config: channel._id,
-         //    //    allowedUsers: {$in}
-         //    // }).exec();
-
-         //    // allPolls = allPolls.concat(polls);
-         // }
-
-         // l({ channelMap });
 
          const newUser = await User.createUser(voterId, eligibleChannels);
-         // const newUser = await new User({
-         //    _id: new Types.ObjectId(),
-         //    discordId: voterId,
-         //    eligibleChannels: channelMap,
-         // }).save();
-
          l({ newUser });
 
-         // allPolls.
 
+         const participation = await newUser.participation(channelId);
 
-         const channelParticipation = await newUser.participation(channelId);
+          const header = `**#${channelName} | ${
+             nickname ?? username
+            }#${discriminator} vote participation**\n`;
 
-         // l({ allPolls });
+          const stats = codeBlock(
+             `${witnessed} votes witnessed\n${participated} votes participated\n${participation} participation rate`
+         );
+
+         const response = `${header}${stats}`;
 
          return interaction.reply({
-            content: channelParticipation,
+            content: response,
             ephemeral: true,
          });
       } else {
          const participation = await voterDoc.participation(channelId);
 
-         interaction.reply({ content: participation, ephemeral: true });
+         const header = `**#${channelName} | ${
+            nickname ?? username
+            }#${discriminator} vote participation**\n`;
+
+         const stats = codeBlock(
+            `${witnessed} votes witnessed\n${participated} votes participated\n${participation} participation rate`
+         );
+
+         const response = `${header}${stats}`;
+
+
+         interaction.reply({ content: response, ephemeral: true });
       }
-
-      // l({ voterId });
-      // // l({ member });
-      // // l(member.roles.cache);
-      // // l({ memberRoles });
-      // l([...memberRoles.keys()]);
-      // l('MORBOOOOOOOOO');
-
-      // // todo see if I can add this into one of the Schemas to make it reusable
-      // const hasVotingRole = await PollChannel.countDocuments({
-      //    allowedRoles: { $in: [...memberRoles.keys()] },
-      // });
-
-      // if (!hasVotingRole)
-      //    throw new Error(
-      //       'This member has no voting roles. There participation can not be gauged, because they are unable to participate.'
-      //    );
-
-      // l({ hasVotingRole });
-
-      // l(mCache.get(voterId));
-
-      // const voterDoc = await User.find().byDiscordId(voterId).exec();
-
-      // if (!voterDoc.length) throw new Error('No users found');
-
-      // l({ voterDoc });
-
-      // return interaction.reply({ content: 'Aborting Early', ephemeral: true });
-
-      // check user db to see if ther user already exists inside the collection
-
-      // user.checkExists ?
-      // 1) no - create user
-      // 2) yes - retrieve participation
-
-      // 1) create user:
-      // > query all channelConfigs
-      // > compare the the user roles against the allowedRoles from the channelConfigs
-      // > store configs that mark user as voter
-      // > query polls attached to stored configs and filter results for polls in which the target user was keyed into the allowedVoter map
-
-      // wait wait, hold on... Let's see if it's possible to simply count the documents in which the user allowed to participate, and count the documents in which the user was not set as vote? This is becoming another optimization thing... dammit Time to get up in dem guts I see what hap(pen15) I guess
    },
 };
