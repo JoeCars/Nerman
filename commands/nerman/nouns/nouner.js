90cac0604b24e9adf803bbcc1137aaea86ee3671
JulesRuby <JulesRuby@users.noreply.github.com>
2022-12-16 05:02:35 -0700

    updated the nouner command to match the design file and leverage to SON library to fetch nouner data.

--- commands/nerman/nouns/nouner.js
+++ commands/nerman/nouns/nouner.js
@@ -1,6 +1,9 @@
 const { CommandInteraction } = require('discord.js');
 const fetch = require('node-fetch');
 const nounpic = require(`../../../helpers/nounpic.js`);
+const getNounerInfo = require('../../../helpers/nouns/getNounerInfo');
+const createNounerEmbed = require('../../../helpers/nouns/createNounerEmbed');
+const { log: l, error: lerr } = console;
 
 module.exports = {
    subCommand: 'nerman.nouner',
@@ -9,10 +12,16 @@ module.exports = {
     * @param {CommandInteraction} interaction
     */
    async execute(interaction) {
+      const Nouns = interaction.client.libraries.get('Nouns');
+      await interaction.deferReply({ ephemeral: true });
+
+      // fixme I can take out these regexp now that the SON library handles that for me -- UPDATED:: Maybe I'll keep them in for now, because even though the library's regexp works. It only returns Null, which may not be sufficient for giving the user adequate feedback on why the command threw an error.
       const walletRegex = /^0x[a-fA-F0-9]{40}$/;
       const ensRegex = /^.*\.eth$/;
 
       const queryTarget = interaction.options.getString('target');
+
+      // disabled until we can find a solution for outputting the tile in the embed
       const includeDelegates =
          interaction.options.getBoolean('delegates') ?? false;
 
@@ -22,25 +31,36 @@ module.exports = {
          );
       }
 
-      const resp = await fetch(
-         `https://noun.pics/${queryTarget}?includeDelegates=${includeDelegates}`
-      );
+      const nounerInfo = await getNounerInfo(Nouns, queryTarget);
+      l('NOUNER INFO\n', nounerInfo);
 
-      if (!resp.ok) {
-         throw new Error(
-            `Unable to return any tile connected to ${queryTarget}, are you sure this nouner exists?`
-         );
-      }
+      // disabled we're not currently using this image url for an attachment of this tile until we find a better solution vvvvv
+      // const resp = await fetch(
+      //    `https://noun.pics/${queryTarget}?includeDelegates=${includeDelegates}`
+      // );
 
-      const msgAttach = await nounpic.fetchNouner(
-         queryTarget,
-         includeDelegates
-      );
+      // const attachUrl = `https://noun.pics/${queryTarget}?includeDelegates=${includeDelegates}`;
 
-      await interaction.reply({
-         content: `Retrieving tile of nouns belonging to ${queryTarget}`,
-         files: [msgAttach],
-         ephemeral: true,
+      // if (!resp.ok) {
+      //    throw new Error(
+      //       `Unable to return any tile connected to ${queryTarget}, are you sure this nouner exists?`
+      //    );
+      // }
+
+      // const msgAttach = await nounpic.fetchNouner(
+      //    queryTarget,
+      //    includeDelegates
+      // );
+
+      // disabled we're not currently using this image url for an attachment of this tile until we find a better solution ^^^^^^^
+
+
+      const nounerEmbed = await createNounerEmbed(nounerInfo);
+
+      await interaction.editReply({
+         // content: `Retrieving tile of nouns belonging to ${queryTarget}`,
+         embeds: [nounerEmbed],
+         // ephemeral: true,
       });
    },
 };
