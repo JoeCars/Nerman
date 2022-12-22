67c36e3a8c7055d2726f164d90fcab11551bd371
JulesRuby <JulesRuby@users.noreply.github.com>
2022-12-21 19:46:49 -0700

    Changed this command to make use of Joel's new spec

--- helpers/nouns/createNounerEmbed.js
+++ helpers/nouns/createNounerEmbed.js
@@ -1,34 +1,58 @@
 const { MessageEmbed } = require('discord.js');
+const { hyperlink, codeBlock } = require('@discordjs/builders');
 const { log: l } = console;
 
 module.exports = async data => {
    try {
-      const { address, ens, owned, delegated } = data;
+      // const { address, ens, owned, delegated } = data;
+
+      const {
+         addressPrint,
+         ownerVotingPower,
+         ownerNounsOwned,
+         ownerNounsDelegated,
+         delegateAddressPrint,
+         delegateVotingPower,
+         delegateNounsOwned,
+         delegateNounsDelegated,
+      } = data;
 
       // Links:
-      const agora = `[Agora](https://www.nounsagora.com/delegate/${address})`;
-      const collective = `[Collective](https://collective.xyz/nouns/@${address})`;
-      const context = `[Context](https://context.app/${ens ?? address})`;
+
+      // const agora = `[Agora](https://www.nounsagora.com/delegate/${addressPrint})`;
+      // const collective = `[Collective](https://collective.xyz/nouns/@${addressPrint})`;
+      // const context = `[Context](https://context.app/${addressPrint})`;
+
+      const ownerHyperlink = hyperlink(
+         addressPrint,
+         `https://etherscan.io/address/${addressPrint}`
+      );
+      const delegateHyperlink = hyperlink(
+         delegateAddressPrint,
+         `https://etherscan.io/address/${delegateAddressPrint}`
+      );
+
+      const addressCodeBlock = codeBlock(
+         `Voting Power: ${ownerVotingPower}\n----------------\n - owned: ${ownerNounsOwned}\n - delegations: ${ownerNounsDelegated}`
+      );
+      const delegateCodeBlock = codeBlock(
+         `Voting Power: ${delegateVotingPower}\n----------------\n - owned: ${delegateNounsOwned}\n - delegations: ${delegateNounsDelegated}`
+      );
 
       const nounerEmbed = new MessageEmbed().setColor('#00FFFF').addFields(
          {
-            name: `**NOUNER**`,
-            value: `[${ens}](https://etherscan.io/address/${address})\n\u200B\u200B`,
+            name: `**ADDRESS**`,
+            // value: `[${addressPrint}](https://etherscan.io/address/${address})\n\u200B\u200B`,
+            value: `${ownerHyperlink}\n${addressCodeBlock}\n${ownerHyperlink} is delegating ${ownerNounsDelegated} votes to ${delegateHyperlink}\n\u200B`,
          },
          {
-            name: `**NOUNS**`,
-            value: `Nouns Owned: ${owned}\nNouns Delegated: ${delegated}\n---\nVoting Power: ${
-               owned + delegated
-            }\n\u200B\u200B`,
-         },
-         {
-            name: `**LINKS**`,
-            value: `${agora}, ${collective}, ${context}`,
+            name: `**DELEGATE**`,
+            value: `${delegateHyperlink}\n${delegateCodeBlock}`,
          }
       );
 
       return nounerEmbed;
    } catch (err) {
-      console.log({ err });
+      l({ err });
    }
 };
