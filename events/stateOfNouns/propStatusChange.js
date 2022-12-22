f404e599a2344972e025c2f871beafcc3bb73145
JulesRuby <JulesRuby@users.noreply.github.com>
2022-12-22 11:37:49 -0700

    Adding nouns-giv channel output for propStatus change

---
+++ events/stateOfNouns/propStatusChange.js
@@ -0,0 +1,115 @@
+const { MessageEmbed, Message } = require('discord.js');
+const { inlineCode } = require('@discordjs/builders');
+
+const Poll = require('../../db/schemas/Poll');
+
+const shortenAddress = require('../../helpers/nouns/createNounEmbed');
+
+const { log: l } = console;
+
+const nounsGovId = process.env.NOUNS_GOV_ID;
+
+module.exports = {
+   name: 'propStatusChange',
+   /**
+    * @param {Message} interaction
+    */
+   async execute(message, statusChange, data) {
+      try {
+         l('PROP STATUS CHANGE EVENT HANDLER');
+
+         const {
+            guild: {
+               channels: { cache },
+            },
+         } = message;
+
+         const { id: proposalId } = data;
+         l('PROP STATUS CHANGE EVENT HANDLER');
+
+         const Nouns = await message.client.libraries.get('Nouns');
+         const nounsGovChannel = await cache.get(nounsGovId);
+
+         l({ message });
+         l({ statusChange });
+         l({ data });
+         l({ nounsGovChannel });
+
+         const propRegExp = new RegExp(`^prop\\s${Number(proposalId)}`, 'i');
+
+         const targetPolls = await Poll.find({
+            'pollData.title': { $regex: propRegExp },
+         });
+         // Poll.find({ 'pollData.title': { $regex: propRegExp })
+         const targetPoll = await Poll.findOne({
+            'pollData.title': { $regex: propRegExp },
+         });
+
+         l({ targetPolls });
+         l({ targetPoll });
+
+         const titleFromFind = targetPolls[0].pollData.title;
+         const titleFromFindOne = targetPoll.pollData.title;
+
+         l({ titleFromFind });
+         l({ titleFromFindOne });
+
+         // const { id } = data;
+         // const titleRegex = new RegExp(/^#+\s+.+\n/);
+
+         // const titleRegex = new RegExp(
+         //    /^(\#\s((\w|[0-9_\-+=.,!:`~%;_&$()*/\[\]\{\}@\\\|])+\s+)+(\w+\s?\n?))/
+         // );
+
+         // const title = description
+         //    .match(titleRegex)[0]
+         //    .replaceAll(/^(#\s)|(\n+)$/g, '');
+         // l({ title });
+
+         // const titleHyperlink = `Prop ${proposalId} - [${voter}](https://nouns.wtf/vote/${proposalId})`;
+         // const description = `https://nouns.wtf/vote/${propId}`;
+         // l({ titleHyperlink });
+
+         // const intRegex = new RegExp(/^\d*$/);
+
+         // console.log({ everyoneId });
+         // console.log(channelConfig.allowedRoles);
+
+         // const messageObject = await initPollMessage({
+         //    propId,
+         //    title,
+         //    description,
+         //    channelConfig,
+         //    everyoneId,
+         // });
+
+         // console.log(
+         //    '-----------------------------------------------\n',
+         //    messageObject.embeds,
+         //    '-----------------------------------------------\n'
+         // );
+
+         const voteEmbedFind = new MessageEmbed()
+            .setTitle(titleFromFind)
+            .setDescription(
+               `https://nouns.wtf/vote/${proposalId}\n${statusChange}`
+            );
+         const voteEmbedFindOne = new MessageEmbed()
+            .setTitle(titleFromFindOne)
+            .setDescription(
+               `https://nouns.wtf/vote/${proposalId}\n${statusChange}`
+            );
+
+         l('VOTE EMBED FIND\n', { voteEmbedFind });
+         l('VOTE EMBED FIND ONE\n', { voteEmbedFindOne });
+
+         // return await message.edit({ content: null, embeds: [voteEmbedFind] });
+         return await message.edit({
+            content: null,
+            embeds: [voteEmbedFindOne],
+         });
+      } catch (error) {
+         console.error(error);
+      }
+   },
+};
