14d4da34db6ce489f98de29bd8c4d48de6952320
JulesRuby <JulesRuby@users.noreply.github.com>
2023-02-06 04:11:24 -0700

    Added in stuff to do the thing... evaluate against whitelist of guild IDs and if it's not in the whitelist, Nerman will leave the server.

--- events/guild/guildJoin.js
+++ events/guild/guildJoin.js
@@ -1,5 +1,5 @@
 const { Guild } = require('discord.js');
-const { log: l} = console;
+const { log: l } = console;
 
 module.exports = {
    name: 'guildCreate',
@@ -8,6 +8,29 @@ module.exports = {
     * @param {Guild} guild
     */
    async execute(guild) {
-      console.log('NEW GUILD JOINED', guild);
+      // TESTING GUILD ID: 783406052372643940
+      const { id: guildId } = guild;
+
+      // todo this is temporary until I can just add a proper whitelist env to heroku, which first requires a (now) mandatory 2FA to log in, which I need Joel to handle.
+      const guildWhitelist =
+         process.env.NODE_ENV === 'development'
+            ? process.env.GUILD_ID_WHITELIST.split(',')
+            : ['919783277726957599', '992462546860785778'];
+
+      l({ guildWhitelist });
+      l('CLIENT', guild.client);
+      l('NEW GUILD JOINED', guild);
+      l('NEW GUILD ID:', guildId);
+
+      // l('CHANNELS', channels.cache);
+
+      if (!guildWhitelist.includes(guildId)) {
+         l('THIS GUILD IS FULL OF STUPID POSEURS!');
+         l('LEAVING GUILD...');
+
+         await guild.leave();
+      } else {
+         l('THIS GUILD IS WHITELISTED');
+      }
    },
 };
