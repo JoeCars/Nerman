7cb405a718931a4cd30d3e5f9c542b9ee9c47329
JulesRuby <JulesRuby@users.noreply.github.com>
2023-02-13 07:59:07 -0700

    Added in guildId and new validators to ensure unique users in between guilds+channels

--- db/schemas/User.js
+++ db/schemas/User.js
@@ -6,11 +6,30 @@ const { log: l } = console;
 const userSchema = new Schema(
    {
       _id: Schema.Types.ObjectId,
+      // discordId: {
+      //    type: String,
+      //    required: true,
+      //    unique: true,
+      // },
+      guildId: { type: String, required: true },
       discordId: {
          type: String,
          required: true,
-         unique: true,
+         // unique: true,
+         validate: {
+            validator: async function (discordId) {
+               const userExists = await this.schema.statics.userExists(
+                  this.guildId,
+                  discordId
+               );
+
+               l({ userExists });
+
+               return !userExists;
+            },
+         },
       },
+      // todo I'm going to need to maybe add in a guildId to this to differentiate users... or perhaps change the eligible channels map to have guildId as parent keys and then channels as a sub-map to those keys
       nameHistory: {
          type: [String],
       },
@@ -29,7 +48,7 @@ const userSchema = new Schema(
       // comment
       timestamps: { createdAt: 'timeCreated', updatedAt: 'modified' },
       statics: {
-         async createUser(voterId, eligibleChannels) {
+         async createUser(guildId, voterId, eligibleChannels) {
             const eligibleMap = new Map();
 
             for (const channel of eligibleChannels) {
@@ -56,6 +75,7 @@ const userSchema = new Schema(
 
             return await this.create({
                _id: new Types.ObjectId(),
+               guildId: guildId,
                discordId: voterId,
                eligibleChannels: eligibleMap,
             });
@@ -82,6 +102,23 @@ const userSchema = new Schema(
             l(this.schema.statics);
             l(this.schema.methods);
             l(this.schema.query);
+         },
+         async userExists(guildId, discordId) {
+            l('FROM STATIC');
+
+            l({ guildId });
+            l({ discordId });
+            // const userDoc = await this.findOne()
+            //    .byDiscordId(guildId, discordId)
+            //    .exec();
+            const userDoc = await model('User')
+               .findOne()
+               .byDiscordId(discordId, guildId)
+               .exec();
+
+            l({ userDoc });
+
+            return userDoc;
          },
       },
       methods: {
@@ -139,9 +176,12 @@ const userSchema = new Schema(
          },
       },
       query: {
-         byDiscordId(discordId) {
+         byDiscordId(discordId, guildId) {
             try {
-               return this.where({ discordId: new RegExp(discordId, 'i') });
+               return this.where({
+                  discordId: new RegExp(discordId, 'i'),
+                  guildId: new RegExp(guildId, 'i'),
+               });
             } catch (error) {
                console.trace({ error });
                throw new Error(
