319ccfad0b0734419d5201dcf83fa506b18dfda3
JulesRuby <JulesRuby@users.noreply.github.com>
2023-02-15 12:01:05 -0700

    Added in new dummy data for emitting proposal test transfers

--- dummyData/sonEventData.js
+++ dummyData/sonEventData.js
@@ -5,10 +5,14 @@ module.exports = (() => {
    eventDataMap.set('auctionBid', {
       id: 612,
       bidder: { id: '0xC66AcE54a394f9Cd2D9EA94C9Ede671D86C44479' },
-      amount: '1770000000000000000',
-      extended: false
+      amount: 1770000000000000000,
+      extended: false,
    });
-   eventDataMap.set('transferNouns', null);
+   eventDataMap.set('transferNoun', {
+      from: { id: '0x5606B493c51316A9e65c9b2A00BbF7Ff92515A3E' },
+      to: { id: '0x3004E7d0bA11BcD506349F1062eA57f7037F0BBd' },
+      tokenId: 608,
+   });
 
    return eventDataMap;
 })();
