0c2654864ab6b6fab56d5fe14f25f08e5118c639
JulesRuby <JulesRuby@users.noreply.github.com>
2023-02-20 06:12:11 -0700

    Added some functions for twotter sharing back in. Added a try/catch to log any additional errors and fail gracefully - I hope.

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -47,110 +47,116 @@ module.exports = {
          },
       } = reaction;
 
-      console.log('stahp => ',stahp)
+      try {
 
-      if (stahp === true) {
-         return;
-      }
+         console.log('stahp => ', stahp)
 
-      l(process.env.DEPLOY_STAGE);
-      l({ nermanEmojiId });
-      l({ emojiId });
-
-      // if (process.env.DEPLOY_STAGE === 'staging') return;
-      if (emojiId !== nermanEmojiId) {
-         l(`This is not MY nerman --- ${process.env.DEPLOY_STAGE}`);
-         return;
-      } else {
-         l(`MY BABY NERMAN --- ${process.env.DEPLOY_STAGE}`);
-      }
-
-      const authorName = membersCache.get(id).nickname ?? username;
-
-      // console.log({ reaction });
-      console.log({ emoji });
-      console.log(emoji.name);
-      console.log(emoji.id); // yus
-      console.log(emoji.identifier); // maybe yus
-      // console.log(await emoji.toString());
-      // console.log(await reaction.fetch());
-      // console.log(reaction.message.reactions);
-      // console.log(reactionsCache);
-      // console.log(await reactionsCache.get());
-
-      // return;
-
-      // below code to calculate voteThreshold should be refactored with threshold.js code into nThreshold.js
-
-      // const Role = rolesCache.find(role => role.name == 'Voters');
-      const Role = rolesCache.find(role => role.id === allowedRoles);
-
-      let votersOnline = membersCache
-         .filter(member => member.presence?.status == 'online')
-         .filter(member => member.roles.cache.find(role => role == Role)).size;
-
-      let voteThreshold = nThreshold.getThreshold(votersOnline);
-
-      let msgAttachmentUrls = [];
-
-      let mappedMentions = {};
-      // let mentions = reaction.message.mentions.members;
-
-      mentionsMembers.forEach(mention => {
-         mappedMentions[mention.user.id] =
-            mention.nickname ?? mention.user.username;
-      });
-
-      // disabled
-      // let tweetContent = await nTwitter.formatTweet(
-      //    content,
-      //    authorName,
-      //    mappedMentions
-      // );
-
-      // let messageTweeted = await reactionsCache.get('931919315010220112'); //check for NermanBlast
-      let messageTweeted = await reactionsCache.get('932664888642400276'); //check for NermanBlast
-
-      // console.log(
-      //    'Which is Nerman,',
-      //    await reactionsCache.get('931919315010220112') // THis is nothing, apparently?
-      // );
-      // console.log(
-      //    'and which is Blast?',
-      //    await reactionsCache.get('932664888642400276')
-      // );
-
-      if (attachments.size > 0) {
-         for (const attachment of attachments) {
-            msgAttachmentUrls.push(attachment[1].url);
+         if (stahp === true) {
+            return;
          }
+
+         l(process.env.DEPLOY_STAGE);
+         l({ nermanEmojiId });
+         l({ emojiId });
+
+         // if (process.env.DEPLOY_STAGE === 'staging') return;
+         if (emojiId !== nermanEmojiId) {
+            l(`This is not MY nerman --- ${process.env.DEPLOY_STAGE}`);
+            return;
+         } else {
+            l(`MY BABY NERMAN --- ${process.env.DEPLOY_STAGE}`);
+         }
+
+         const authorName = membersCache.get(id).nickname ?? username;
+
+         // console.log({ reaction });
+         console.log({ emoji });
+         console.log(emoji.name);
+         console.log(emoji.id); // yus
+         console.log(emoji.identifier); // maybe yus
+         // console.log(await emoji.toString());
+         // console.log(await reaction.fetch());
+         // console.log(reaction.message.reactions);
+         // console.log(reactionsCache);
+         // console.log(await reactionsCache.get());
+
+         // return;
+
+         // below code to calculate voteThreshold should be refactored with threshold.js code into nThreshold.js
+
+         // const Role = rolesCache.find(role => role.name == 'Voters');
+         const Role = rolesCache.find(role => role.id === allowedRoles);
+
+         let votersOnline = membersCache
+            .filter(member => member.presence?.status == 'online')
+            .filter(member => member.roles.cache.find(role => role == Role)).size;
+
+         let voteThreshold = nThreshold.getThreshold(votersOnline);
+
+         let msgAttachmentUrls = [];
+
+         let mappedMentions = {};
+         // let mentions = reaction.message.mentions.members;
+
+         mentionsMembers.forEach(mention => {
+            mappedMentions[mention.user.id] =
+               mention.nickname ?? mention.user.username;
+         });
+
+         // enabled
+         let tweetContent = await nTwitter.formatTweet(
+            content,
+            authorName,
+            mappedMentions
+         );
+
+         // let messageTweeted = await reactionsCache.get('931919315010220112'); //check for NermanBlast
+         let messageTweeted = await reactionsCache.get('932664888642400276'); //check for NermanBlast
+
+         // console.log(
+         //    'Which is Nerman,',
+         //    await reactionsCache.get('931919315010220112') // THis is nothing, apparently?
+         // );
+         // console.log(
+         //    'and which is Blast?',
+         //    await reactionsCache.get('932664888642400276')
+         // );
+
+         if (attachments.size > 0) {
+            for (const attachment of attachments) {
+               msgAttachmentUrls.push(attachment[1].url);
+            }
+         }
+
+         if (
+            embeds.length &&
+            embeds[0].provider.name === 'Tenor' &&
+            content === embeds[0].url
+         ) {
+            const tenorURL = await tenor.getUrl(embeds);
+            tweetContent = tweetContent.replace(embeds[0].url, '');
+            msgAttachmentUrls.push(tenorURL);
+         }
+
+         // if (
+         //    !messageTweeted &&
+         //    reaction.emoji.name == 'Nerman' &&
+         //    reaction.count > voteThreshold - 1
+         // ) {
+         // enabled
+         if (
+            !messageTweeted &&
+            emojiId === nermanEmojiId &&
+            reaction.count >= voteThreshold
+         ) {
+            nTwitter.post(tweetContent, msgAttachmentUrls);
+
+            // mark message with NermanBlast emoji
+            await reaction.message.react('932664888642400276');
+         }
+      } catch (error) {
+         console.error('messageReactionAdd -----------  AW BEANZ')
+         console.error(error)
       }
-
-      if (
-         embeds.length &&
-         embeds[0].provider.name === 'Tenor' &&
-         content === embeds[0].url
-      ) {
-         const tenorURL = await tenor.getUrl(embeds);
-         tweetContent = tweetContent.replace(embeds[0].url, '');
-         msgAttachmentUrls.push(tenorURL);
-      }
-
-      // if (
-      //    !messageTweeted &&
-      //    reaction.emoji.name == 'Nerman' &&
-      //    reaction.count > voteThreshold - 1
-      // ) {
-      // disabled
-      // if (
-      //    !messageTweeted &&
-      //    emojiId === nermanEmojiId &&
-      //    reaction.count >= voteThreshold
-      // ) {
-      //    nTwitter.post(tweetContent, msgAttachmentUrls);
-
-      //    // mark message with NermanBlast emoji
-      //    await reaction.message.react('932664888642400276');
-      // }
    },
 };
