541e4a1d7a61f83cd1d62782d01a155b57eef586
JulesRuby <JulesRuby@users.noreply.github.com>
2023-04-28 14:22:15 -0600

    Working in the live results stuff, bad commit message is bad

--- events/poll/pollVote.js
+++ events/poll/pollVote.js
@@ -4,12 +4,20 @@ const {
    ModalSubmitInteraction,
    MessageEmbed,
 } = require('discord.js');
-const { userMention, inlineCode, hyperlink } = require('@discordjs/builders');
+const {
+   userMention,
+   inlineCode,
+   codeBlock,
+   hyperlink,
+} = require('@discordjs/builders');
 
 const { Types } = require('mongoose');
 const Poll = require('../../db/schemas/Poll');
 const User = require('../../db/schemas/User');
 const Vote = require('../../db/schemas/Vote');
+
+const { longestString } = require('../../helpers/poll');
+const ResultBar = require('../../classes/ResultBar');
 
 const nouncilId = process.env.TESTNERMAN_NOUNCIL_CHAN_ID;
 const jtsNouncilId = process.env.JTS_NOUNCIL_ID;
@@ -160,9 +168,94 @@ module.exports = {
 
       votingUser.incParticipation(channelId);
 
+      /**
+       *
+       *
+       *
+       *
+       *
+       *
+       */
+
+      // todo I need to extract this chunk into a module
+
+      const results = updatedPoll.results;
+      const longestOption = longestString(updatedPoll.pollData.choices).length;
+
+      console.log(
+         'events/poll/pollVote.js -- longestOption => ',
+         longestOption
+      );
+      // let resultsArray = ['```', '```'];
+      let resultsArray = pollStatus.config.voteThreshold
+         ? [
+              `Threshold: ${updatedPoll.voteThreshold} ${
+                 updatedPoll.voteThreshold > 1 ? 'votes' : 'vote'
+              }\n`,
+           ]
+         : [];
+
+      let resultsOutput = [];
+
+      const barWidth = 8;
+      let totalVotes = results.totalVotes;
+      // let totalVotes = updatedPoll.results.totalVotes;
+
+      let votesMap = new Map([
+         ['maxLength', barWidth],
+         ['totalVotes', totalVotes],
+      ]);
+      for (const key in results.distribution) {
+         const label = key[0].toUpperCase() + key.substring(1);
+
+         console.log('db/index.js -- label => ', label);
+         console.log('db/index.js -- label.length => ', label.length);
+         console.log(
+            'db/index.js -- logging :  longestOption - label.length => ',
+            longestOption - label.length
+         );
+         const votes = results.distribution[key];
+         const room = longestOption - label.length;
+         let optionObj = new ResultBar(label, votes, room, votesMap);
+
+         console.log('optionObj => ', optionObj);
+         console.log('optionObj.completeBar => ', optionObj.completeBar);
+
+         votesMap.set(label, optionObj);
+         // resultsArray.splice(-1, 0, optionObj.completeBar);
+         resultsArray.push(optionObj.completeBar);
+      }
+
+      resultsArray.push(`\nAbstains: ${updatedPoll.abstains.size}`);
+
+      // console.log(votesMap);
+
+      // resultsOutput = resultsArray.join('\n');
+      resultsOutput = codeBlock(resultsArray.join('\n'));
+
+      /**
+       *
+       *
+       *
+       *
+       *
+       *
+       *
+       *
+       *
+       */
+
       let message = await client.channels.cache
          .get(channelId)
          .messages.fetch(messageId);
+
+      l('events/poll/pollVote.js  -- resultsOutput => ', resultsOutput);
+      l('events/poll/pollVote.js  -- updatedPoll => ', updatedPoll);
+      l('events/poll/pollVote.js  -- pollOptions.config => ', pollOptions);
+      l(
+         'events/poll/pollVote.js  -- pollOptions.liveVisualFeed => ',
+         pollOptions.liveVisualFeed
+      );
 
       const updateEmbed = new MessageEmbed(message.embeds[0]);
 
@@ -175,6 +268,25 @@ module.exports = {
             inline: true,
          }
       );
+
+      l('events/poll/pollVote.js  -- updateEmbed => ', updateEmbed);
+      l(
+         'events/poll/pollVote.js  -- updatedPoll.config.liveVisualFeed => ',
+         pollOptions.liveVisualFeed
+      );
+
+      if (pollOptions.liveVisualFeed) {
+         // if (updatedPoll.config.liveVisualFeed) {
+         l(
+            'events/poll/pollVote.js  -- inside if (updatedPoll.config.liveVisualFeed) =>  SUCCESS'
+         );
+
+         updateEmbed.spliceFields(1, 1, {
+            name: 'Results',
+            value: resultsOutput,
+            inline: false,
+         });
+      }
 
       message.edit({ embeds: [updateEmbed] });
 
