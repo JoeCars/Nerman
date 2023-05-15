1326285e373b7008038fdc68170c9095c86d631f
Sobhan <s.kevishahi@gmail.com>
2023-05-15 13:57:32 -0700

    Improved Object Logging

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,27 +1,27 @@
 class Logger {
    static info(message, context) {
       let date = new Date().toISOString();
-      console.info(`[${date}] [INFO] ${message} ${context}`);
+      console.info(`[${date}] [INFO] ${message} ${JSON.stringify(context)}`);
    }
 
    static debug(message, context) {
       let date = new Date().toISOString();
-      console.debug(`[${date}] [DEBUG] ${message} ${context}`);
+      console.debug(`[${date}] [DEBUG] ${message} ${JSON.stringify(context)}`);
    }
 
    static warn(message, context) {
       let date = new Date().toISOString();
-      console.warn(`[${date}] [WARN] ${message} ${context}`);
+      console.warn(`[${date}] [WARN] ${message} ${JSON.stringify(context)}`);
    }
 
    static error(message, context) {
       let date = new Date().toISOString();
-      console.error(`[${date}] [ERROR] ${message} ${context}`);
+      console.error(`[${date}] [ERROR] ${message} ${JSON.stringify(context)}`);
    }
 
    static fatal(message, context) {
       let date = new Date().toISOString();
-      console.error(`[${date}] [FATAL] ${message} ${context}`);
+      console.error(`[${date}] [FATAL] ${message} ${JSON.stringify(context)}`);
    }
 }
 
