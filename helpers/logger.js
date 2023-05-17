61f3951393a90f2ac43b818d8cfdceebc104dd1b
Sobhan <s.kevishahi@gmail.com>
2023-05-17 09:32:12 -0700

    Added Default Context

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,25 +1,25 @@
 class Logger {
-   static info(message, context) {
+   static info(message, context = {}) {
       let date = new Date().toISOString();
       console.info(`[${date}] [INFO] ${message} ${JSON.stringify(context)}`);
    }
 
-   static debug(message, context) {
+   static debug(message, context = {}) {
       let date = new Date().toISOString();
       console.debug(`[${date}] [DEBUG] ${message} ${JSON.stringify(context)}`);
    }
 
-   static warn(message, context) {
+   static warn(message, context = {}) {
       let date = new Date().toISOString();
       console.warn(`[${date}] [WARN] ${message} ${JSON.stringify(context)}`);
    }
 
-   static error(message, context) {
+   static error(message, context = {}) {
       let date = new Date().toISOString();
       console.error(`[${date}] [ERROR] ${message} ${JSON.stringify(context)}`);
    }
 
-   static fatal(message, context) {
+   static fatal(message, context = {}) {
       let date = new Date().toISOString();
       console.error(`[${date}] [FATAL] ${message} ${JSON.stringify(context)}`);
    }
