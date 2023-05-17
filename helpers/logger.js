586210f3048f1ad2d099606288130d7742fad341
Sobhan <s.kevishahi@gmail.com>
2023-05-17 11:42:11 -0700

    Parsed Context For Extra Information

--- helpers/logger.js
+++ helpers/logger.js
@@ -1,27 +1,67 @@
+function isEmptyObject(object) {
+   return Object.keys(object).length === 0;
+}
+
+function checkForErrors(context) {
+   if (context.error) {
+      console.error(context.error);
+   } else if (context.err) {
+      console.error(context.err);
+   }
+}
+
+function stringifyContext(context) {
+   if (!context || isEmptyObject(context)) {
+      return '';
+   }
+
+   try {
+      return JSON.stringify(context);
+   } catch (error) {
+      return 'Unable to stringify context due to an error. ' + error.message;
+   }
+}
+
 class Logger {
    static info(message, context = {}) {
       let date = new Date().toISOString();
-      console.info(`[${date}] [INFO] ${message} ${JSON.stringify(context)}`);
+      let contextString = stringifyContext(context);
+
+      console.info(`[${date}] [INFO] ${message} ${contextString}`);
    }
 
    static debug(message, context = {}) {
       let date = new Date().toISOString();
-      console.debug(`[${date}] [DEBUG] ${message} ${JSON.stringify(context)}`);
+      let contextString = stringifyContext(context);
+
+      console.debug(`[${date}] [DEBUG] ${message} ${contextString}`);
    }
 
    static warn(message, context = {}) {
       let date = new Date().toISOString();
-      console.warn(`[${date}] [WARN] ${message} ${JSON.stringify(context)}`);
+      let contextString = stringifyContext(context);
+
+      console.warn(`[${date}] [WARN] ${message} ${contextString}`);
+
+      checkForErrors(context);
    }
 
    static error(message, context = {}) {
       let date = new Date().toISOString();
-      console.error(`[${date}] [ERROR] ${message} ${JSON.stringify(context)}`);
+      let contextString = stringifyContext(context);
+
+      console.error(`[${date}] [ERROR] ${message} ${contextString}`);
+
+      checkForErrors(context);
    }
 
    static fatal(message, context = {}) {
       let date = new Date().toISOString();
-      console.error(`[${date}] [FATAL] ${message} ${JSON.stringify(context)}`);
+      let contextString = stringifyContext(context);
+
+      console.error(`[${date}] [FATAL] ${message} ${contextString}`);
+
+      checkForErrors(context);
    }
 }
 
