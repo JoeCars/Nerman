8936806ccca181a4b588a7688998b523819ee94b
Sobhan <s.kevishahi@gmail.com>
2023-06-05 14:02:59 -0700

    Implemented `drawSpace()` tests.

--- tests/helpers/poll.test.js
+++ tests/helpers/poll.test.js
@@ -1,6 +1,6 @@
 const { expect } = require('chai');
 
-const { drawBlock } = require('../../helpers/poll');
+const { drawBlock, drawSpace } = require('../../helpers/poll');
 
 describe('helpers/poll.js tests', function () {
    describe('drawBlock() tests', function () {
@@ -30,6 +30,34 @@ describe('helpers/poll.js tests', function () {
          expect(results4).to.equal('███▌');
          expect(results5).to.equal('███▍');
          expect(results6).to.equal('███');
+      });
+   });
+
+   describe('drawSpace() tests', function () {
+      it('should draw empty blocks', function () {
+         const results1 = drawSpace(2);
+         const results2 = drawSpace(4);
+         const results3 = drawSpace(8);
+
+         expect(results1).to.equal('\u200b \u200b ');
+         expect(results2).to.equal('\u200b \u200b \u200b \u200b ');
+         expect(results3).to.equal(
+            '\u200b \u200b \u200b \u200b \u200b \u200b \u200b \u200b ',
+         );
+      });
+
+      it('should draw an empty string when 0', function () {
+         const results1 = drawSpace(0);
+
+         expect(results1).to.equal('');
+      });
+
+      it('should draw an empty string when negative', function () {
+         const results1 = drawSpace(-1);
+         const results2 = drawSpace(-10);
+
+         expect(results1).to.equal('');
+         expect(results2).to.equal('');
       });
    });
 });
