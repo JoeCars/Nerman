fa6959f0afaac1506a40c969fd0d56d4e573abb1
Sobhan <s.kevishahi@gmail.com>
2023-06-06 11:54:17 -0700

    Refactored file.

--- buttons/vote.js
+++ buttons/vote.js
@@ -24,13 +24,8 @@ module.exports = {
       });
 
       if (!interaction.isButton()) {
-         // console.log('VOTE BUTTON -- isButton: false', { interaction });
          return;
       }
-      // console.log(
-      //    'VOTE BUTTON -- isButton: true -- interaction',
-      //    interaction.isButton()
-      // );
 
       const {
          channelId,
@@ -44,100 +39,29 @@ module.exports = {
 
       const { allowedRoles, anonymous: anon } = await PollChannel.findOne(
          { channelId },
-         'allowedRoles'
+         'allowedRoles',
       ).exec();
-
-      if (!anon) {
-         Logger.debug('buttons/vote.js: Checking joined time stamp.', {
-            joinedTimestamp: joinedTimestamp,
-         });
-      }
-
-      // console.log({ allowedRoles });
-
-      if (!roleCache.hasAny(...allowedRoles)) {
-         Logger.info(
-            'buttons/vote.js: User does not have eligible roles to vote on this poll',
-            {
-               userId: interaction.user.id,
-               guildId: interaction.guildId,
-               channelId: interaction.channelId,
-               messageId: interaction.message.id,
-            }
-         );
-         return interaction.reply({
-            content: 'You do not have a role eligible to vote on this poll.',
-            ephemeral: true,
-         });
-      }
 
       const attachedPoll = await Poll.findOne({ messageId })
          .populate([{ path: 'config' }])
          .exec();
 
-      // enabled disabled for testing
-
-      if (attachedPoll.allowedUsers.get(userId) === true) {
-         return interaction.reply({
-            content: 'You have already used up your vote allowance.',
-            ephemeral: true,
-         });
-      }
-
-      if (!attachedPoll.allowedUsers.has(userId)) {
-         return interaction.reply({
-            content: `You are not eligible to participate in polls posted before your arrival:\nPoll posted on: <t:${Math.round(
-               Date.parse(attachedPoll.timeCreated) / 1000
-            )}:F>\nDate you joined: <t:${Math.round(joinedTimestamp / 1000)}>`,
-            ephemeral: true,
-         });
-      }
-
-      // return await interaction.reply({
-      //    content: 'Ending early for testing',
-      //    ephemeral: true,
-      // });
-
-      const capitalizedOptions = attachedPoll.pollData.choices.map(
-         choice => choice[0].toUpperCase() + choice.substring(1)
+      const eligibility = await checkUserEligibility(
+         roleCache,
+         allowedRoles,
+         attachedPoll,
+         userId,
+         joinedTimestamp,
       );
 
-      // const optionsString = attachedPoll.pollData.choices.join(', ');
-      const optionsString = capitalizedOptions.join(', ');
-      // disabled until DJS SELECT MENU Modal supported
-      // const optionsMap = attachedPoll.pollData.choices.map(choice => ({
-      //    label: choice,
-      //    value: choice,
-      // }));
+      if (!eligibility.isEligible) {
+         return interaction.reply({
+            content: eligibility.message,
+            ephemeral: true,
+         });
+      }
 
-      // console.log({ attachedPoll });
-
-      const modal = new Modal().setCustomId('vote-modal').setTitle('Vote');
-
-      const selectOptions = new TextInputComponent()
-         .setCustomId('votingSelect')
-         .setLabel(`Type ${attachedPoll.pollData.voteAllowance} Choice(s)`)
-         .setPlaceholder(optionsString)
-         .setDefaultValue(optionsString)
-         .setStyle('SHORT')
-         .setMaxLength(100)
-         .setRequired(true);
-
-      // disabled until DJS supports SELECT MENU Modal
-      // const selectOptions = new SelectMenuComponent()
-      //    .setCustomId('votingSelect')
-      //    .setPlaceholder('Make your selection(s)')
-      //    .addOptions(optionsMap)
-      //    .setMinValues(attachedPoll.config.voteAllowance)
-      //    .setMaxValues(attachedPoll.config.voteAllowance);
-
-      const reason = new TextInputComponent()
-         .setCustomId('voteReason')
-         .setLabel('Reason')
-         .setPlaceholder('Explain your vote.')
-         .setStyle('LONG');
-
-      modal.addComponents(selectOptions, reason);
+      const modal = createVoteModal(attachedPoll);
 
       try {
          await showModal(modal, {
@@ -158,3 +82,81 @@ module.exports = {
       });
    },
 };
+
+// TODO: Combine this with abstain's checkUserEligibility.
+async function checkUserEligibility(
+   roleCache,
+   allowedRoles,
+   attachedPoll,
+   userId,
+   joinedTimestamp,
+) {
+   if (!roleCache.hasAny(...allowedRoles)) {
+      return {
+         message: 'You do not have a role eligible to vote on this poll.',
+         isEligible: false,
+      };
+   }
+
+   if (attachedPoll.allowedUsers.get(userId) === true) {
+      return {
+         message: 'You have already used up your vote allowance.',
+         isEligible: false,
+      };
+   }
+
+   if (!attachedPoll.allowedUsers.has(userId)) {
+      return {
+         message: `You are not eligible to participate in polls posted before your arrival:\nPoll posted on: <t:${Math.round(
+            Date.parse(attachedPoll.timeCreated) / 1000,
+         )}:F>\nDate you joined: <t:${Math.round(joinedTimestamp / 1000)}>`,
+         isEligible: false,
+      };
+   }
+
+   return {
+      message: 'You are eligible to participate in this pole.',
+      isEligible: true,
+   };
+}
+
+function createVoteModal(attachedPoll) {
+   const capitalizedOptions = attachedPoll.pollData.choices.map(
+      choice => choice[0].toUpperCase() + choice.substring(1),
+   );
+
+   // const optionsString = attachedPoll.pollData.choices.join(', ');
+   const optionsString = capitalizedOptions.join(', ');
+   // disabled until DJS SELECT MENU Modal supported
+   // const optionsMap = attachedPoll.pollData.choices.map(choice => ({
+   //    label: choice,
+   //    value: choice,
+   // }));
+   const modal = new Modal().setCustomId('vote-modal').setTitle('Vote');
+
+   const selectOptions = new TextInputComponent()
+      .setCustomId('votingSelect')
+      .setLabel(`Type ${attachedPoll.pollData.voteAllowance} Choice(s)`)
+      .setPlaceholder(optionsString)
+      .setDefaultValue(optionsString)
+      .setStyle('SHORT')
+      .setMaxLength(100)
+      .setRequired(true);
+
+   // disabled until DJS supports SELECT MENU Modal
+   // const selectOptions = new SelectMenuComponent()
+   //    .setCustomId('votingSelect')
+   //    .setPlaceholder('Make your selection(s)')
+   //    .addOptions(optionsMap)
+   //    .setMinValues(attachedPoll.config.voteAllowance)
+   //    .setMaxValues(attachedPoll.config.voteAllowance);
+
+   const reason = new TextInputComponent()
+      .setCustomId('voteReason')
+      .setLabel('Reason')
+      .setPlaceholder('Explain your vote.')
+      .setStyle('LONG');
+
+   modal.addComponents(selectOptions, reason);
+   return modal;
+}
