185dd0b72a125b05c03da481d7ddc2f2ca89d983
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-04 15:54:44 -0700

    Implemented Thread Status Change event.

---
+++ events/customEvents/poll/threadStatusChange.js
@@ -0,0 +1,36 @@
+const { TextChannel } = require('discord.js');
+
+const Logger = require('../../../helpers/logger');
+const { findPollMessage } = require('../../../helpers/poll/thread');
+const {
+   generateThreadStatusEmbed,
+} = require('../../../views/embeds/threadStatusChange');
+
+module.exports = {
+   name: 'propStatusChange',
+   /**
+    * @param {TextChannel} interaction
+    */
+   async execute(channel, data) {
+      Logger.info(
+         'events/customEvents/poll/threadStatusChange.js: Updating status in poll thread.',
+         {
+            proposalId: `${data.id}`,
+            status: data.status,
+         },
+      );
+
+      const pollMessage = await findPollMessage(channel, data.id);
+
+      if (!pollMessage) {
+         return;
+      }
+
+      const threadEmbed = generateThreadStatusEmbed(data.status);
+
+      await pollMessage.thread.send({
+         content: null,
+         embeds: [threadEmbed],
+      });
+   },
+};
