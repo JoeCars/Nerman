809362a7bc6f8fcbb7772423c67f01aa2fc94f40
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-04 08:39:32 -0700

    Added vote count to the message.

--- views/embeds/delegateChanged.js
+++ views/embeds/delegateChanged.js
@@ -19,28 +19,45 @@ exports.generateDelegateChangedEmbed = async function (
       newDelegate = await findAccountLink(nouns, data.toDelegate.id);
    }
 
+   let voteCount = '';
    try {
       const event = data.event;
       const receipt = await event.getTransactionReceipt();
       const firstData = receipt.logs[0].data;
       const secondData = receipt.logs[1].data;
       const thirdData = receipt.logs[2].data;
+
+      // The number of votes being changes is stored in receipt logs index 1 and 2.
+      // It is formatted as a single hex, where the first 64 digits after 0x is the previous vote count.
+      // And the second 64 digits after 0x is the new vote count of the delegate.
+      // To see this in detail, follow the link of the delegate changed event and check the receipt logs.
+      const usefulData = firstData;
+      const numberSubstring = usefulData.substring(2);
+      const firstHex = numberSubstring.substring(0, 65);
+      const secondHex = numberSubstring.substring(65);
+      const firstNum = parseInt(firstHex, 16);
+      const secondNum = parseInt(secondHex, 16);
+
+      const numOfVotesChanged = Math.abs(firstNum - secondNum);
+
       Logger.debug(
          'views/embeds/delegateChanged.js: Checking transaction receipt data.',
          {
+            numOfVotes: numOfVotesChanged,
             firstData: firstData,
             secondData: secondData,
             thirdData: thirdData,
             logs: receipt.logs,
          },
       );
+      voteCount = `${inlineCode(numOfVotesChanged)} `;
    } catch (error) {
       Logger.error("views/embeds/delegateChanged.js: There's been an error.", {
          error: error,
       });
    }
 
-   const message = `${delegator} delegated votes to ${newDelegate}.`;
+   const message = `${delegator} delegated ${voteCount}votes to ${newDelegate}.`;
 
    const embed = new MessageEmbed().setTitle(title).setDescription(message);
 
