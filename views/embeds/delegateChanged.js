876a47efd9eb8c05b90f97f2f9d5d928ade0b38a
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-05 08:30:48 -0700

    Abstracted vote change extraction logic.

--- views/embeds/delegateChanged.js
+++ views/embeds/delegateChanged.js
@@ -23,30 +23,18 @@ exports.generateDelegateChangedEmbed = async function (
    try {
       const event = data.event;
       const receipt = await event.getTransactionReceipt();
-      const firstData = receipt.logs[0].data;
-      const secondData = receipt.logs[1].data;
-      const thirdData = receipt.logs[2].data;
+      const hexData = receipt.logs[1].data;
 
       // The number of votes being changes is stored in receipt logs index 1 and 2.
       // It is formatted as a single hex, where the first 64 digits after 0x is the previous vote count.
       // And the second 64 digits after 0x is the new vote count of the delegate.
       // To see this in detail, follow the link of the delegate changed event and check the receipt logs.
-      const usefulData = firstData;
-      const numberSubstring = usefulData.substring(2);
-      const firstHex = numberSubstring.substring(0, 65);
-      const secondHex = numberSubstring.substring(65);
-      const firstNum = parseInt(firstHex, 16);
-      const secondNum = parseInt(secondHex, 16);
-
-      const numOfVotesChanged = Math.abs(firstNum - secondNum);
+      const numOfVotesChanged = exports.extractVoteChange(hexData);
 
       Logger.debug(
          'views/embeds/delegateChanged.js: Checking transaction receipt data.',
          {
             numOfVotes: numOfVotesChanged,
-            firstData: firstData,
-            secondData: secondData,
-            thirdData: thirdData,
             logs: receipt.logs,
          },
       );
@@ -66,4 +54,18 @@ exports.generateDelegateChangedEmbed = async function (
    }
 
    return embed;
+};
+
+/**
+ * @param {string} hexData
+ */
+exports.extractVoteChange = function (hexData) {
+   hexData = hexData.substring(2);
+
+   const hex1 = hexData.substring(0, 64);
+   const hex2 = hexData.substring(64);
+   const num1 = parseInt(hex1, 16);
+   const num2 = parseInt(hex2, 16);
+
+   return Math.abs(num1 - num2);
 };
