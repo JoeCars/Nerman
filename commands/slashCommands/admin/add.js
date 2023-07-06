2459c10e56680085ab8d18b52b7a62e79f4f9936
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-06 12:23:43 -0700

    Implemented admin add command.

---
+++ commands/slashCommands/admin/add.js
@@ -0,0 +1,89 @@
+const { CommandInteraction } = require('discord.js');
+const { memberNicknameMention } = require('@discordjs/builders');
+const { Types } = require('mongoose');
+
+const Admin = require('../../../db/schemas/Admin');
+const Logger = require('../../../helpers/logger');
+const { isUserAuthorized } = require('../../../helpers/authorization');
+
+module.exports = {
+   subCommand: 'nerman.admin.add',
+   /**
+    * @param {CommandInteraction} interaction
+    */
+   async execute(interaction) {
+      Logger.info('commands/slashCommands/admin/add.js: Adding a new admin.', {
+         userId: interaction.user.id,
+         guildId: interaction.guildId,
+      });
+
+      if (!isUserAuthorized(interaction.user.id)) {
+         throw new Error('This is an admin-only command');
+      }
+
+      const newAdmin = interaction.options.getUser('user');
+
+      if (!newAdmin) {
+         throw new Error('The user was not supplied.');
+      }
+
+      // Checking for a previously added admin.
+      let numOfConfigs;
+      try {
+         numOfConfigs = await Admin.countDocuments({
+            guildId: interaction.guildId,
+            userId: newAdmin.id,
+         });
+      } catch (error) {
+         Logger.error(
+            'commands/slashCommands/admin/add.js: Unable to search for duplicate.',
+            {
+               error: error,
+            },
+         );
+         throw new Error('Unable to add new admin due to a database issue.');
+      }
+
+      if (numOfConfigs !== 0) {
+         return interaction.reply({
+            ephemeral: true,
+            content:
+               'This user is already registered as an admin in this guild.',
+         });
+      }
+
+      // Inserting new configuration.
+      try {
+         Admin.create({
+            _id: new Types.ObjectId(),
+            guildId: interaction.guildId,
+            userId: newAdmin.id,
+         });
+      } catch (error) {
+         Logger.error(
+            'commands/slashCommands/admin/add.js: Unable to save the new admin.',
+            {
+               error: error,
+            },
+         );
+         throw new Error(
+            'Unable to add the new admin due to a database issue.',
+         );
+      }
+
+      await interaction.reply({
+         ephemeral: true,
+         content: `You have successfully added ${memberNicknameMention(
+            newAdmin.id,
+         )} as a new admin.`,
+      });
+
+      Logger.info(
+         'commands/slashCommands/admin/add.js: Finished adding new admin.',
+         {
+            userId: interaction.user.id,
+            guildId: interaction.guildId,
+         },
+      );
+   },
+};
