95774a2e5c68d68fac3d9ea20216d70cb78a7809
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-06 12:53:03 -0700

    Added new authorization functions.

--- helpers/authorization.js
+++ helpers/authorization.js
@@ -1,5 +1,45 @@
+const { GuildMember, Permissions } = require('discord.js');
+const Admin = require('../db/schemas/Admin');
+const Logger = require('./logger');
+
 exports.isUserAuthorized = function isUserAuthorized(userId) {
    // FIXME: will need to remove these after we figure out a better permission control for admin command
    const authorizedIds = process.env.BAD_BITCHES.split(',');
    return authorizedIds.includes(userId);
+};
+
+/**
+ * @param {GuildMember} user
+ */
+exports.isUserADiscordAdmin = function (user) {
+   return user.permissions.has(Permissions.FLAGS.ADMINISTRATOR);
+};
+
+/**
+ * @param {GuildMember} user
+ */
+exports.isUserANermanAdmin = async function (user) {
+   let count = 0;
+
+   try {
+      count = await Admin.countDocuments({
+         guildId: user.guild.id,
+         userId: user.id,
+      }).exec();
+   } catch (error) {
+      Logger.error('helpers/authorization.js: Unable to search for admins.', {
+         error: error,
+      });
+   }
+
+   return count;
+};
+
+/**
+ * @param {GuildMember} user
+ */
+exports.isUserANermanDeveloper = function (user) {
+   // FIXME: will need to remove these after we figure out a better permission control for admin command
+   const authorizedIds = process.env.BAD_BITCHES.split(',');
+   return authorizedIds.includes(user.id);
 };
