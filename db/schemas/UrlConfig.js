d63660b7e11fd2a5703c8a66e751bfb1e3c19722
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-18 10:27:45 -0700

    Implemented `fetchUrls()`.

--- db/schemas/UrlConfig.js
+++ db/schemas/UrlConfig.js
@@ -1,21 +1,41 @@
 const { Schema, model } = require('mongoose');
 
-const UrlConfigSchema = new Schema({
-   _id: Schema.Types.ObjectId,
-   guildId: {
-      type: String,
-      required: true,
+const DEFAULT_PROPOSAL_URL = 'https://nouns.wtf/vote/';
+const DEFAULT_NOUNS_URL = 'https://nouns.wtf/noun/';
+
+const UrlConfigSchema = new Schema(
+   {
+      _id: Schema.Types.ObjectId,
+      guildId: {
+         type: String,
+         required: true,
+      },
+      propUrl: {
+         type: String,
+         required: true,
+         default: DEFAULT_PROPOSAL_URL,
+      },
+      nounUrl: {
+         type: String,
+         required: true,
+         default: DEFAULT_NOUNS_URL,
+      },
    },
-   propUrl: {
-      type: String,
-      required: true,
-      default: 'https://nouns.wtf/vote/',
+   {
+      statics: {
+         /**
+          * @param {string} guildId
+          */
+         async fetchUrls(guildId) {
+            const config = await this.findOne({ guildId: guildId }).exec();
+
+            return {
+               propUrl: config ? config.propUrl : DEFAULT_PROPOSAL_URL,
+               nounUrl: config ? config.nounUrl : DEFAULT_NOUNS_URL,
+            };
+         },
+      },
    },
-   nounUrl: {
-      type: String,
-      required: true,
-      default: 'https://nouns.wtf/noun/',
-   },
-});
+);
 
 module.exports = model('UrlConfig', UrlConfigSchema);
