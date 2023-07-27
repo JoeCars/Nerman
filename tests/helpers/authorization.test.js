c92adbe2316ab8a2af00dcbfdf7b56c5611f1d99
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-27 09:57:48 -0700

    Renamed developer ids.

--- tests/helpers/authorization.test.js
+++ tests/helpers/authorization.test.js
@@ -5,21 +5,21 @@ const { isUserANermanDeveloper } = require('../../helpers/authorization');
 describe('helpers/authorization.js tests', function () {
    describe('isUserANermanDeveloper() tests', function () {
       it('should return true when string', function () {
-         process.env.BAD_BITCHES = '42,117';
+         process.env.DEVELOPER_IDS = '42,117';
 
          expect(isUserANermanDeveloper('42')).to.be.true;
          expect(isUserANermanDeveloper('117')).to.be.true;
       });
 
       it('should return false when number', function () {
-         process.env.BAD_BITCHES = '42,117';
+         process.env.DEVELOPER_IDS = '42,117';
 
          expect(isUserANermanDeveloper(42)).to.be.false;
          expect(isUserANermanDeveloper(117)).to.be.false;
       });
 
       it('should return false when not part of the list', function () {
-         process.env.BAD_BITCHES = '42,117';
+         process.env.DEVELOPER_IDS = '42,117';
 
          expect(isUserANermanDeveloper(123)).to.be.false;
          expect(isUserANermanDeveloper('123')).to.be.false;
