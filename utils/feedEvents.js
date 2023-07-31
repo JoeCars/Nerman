29fcae9c056f47064793c1ee4dd69afd46035b9f
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-07-31 11:54:28 -0700

    Sorted feed events.

--- utils/feedEvents.js
+++ utils/feedEvents.js
@@ -1,15 +1,16 @@
+// Sorted lexicographically. First by group name. Then by event name.
 const events = new Map();
+events.set('newProposalPoll', 'Nerman.NewProposalPoll');
+events.set('threadStatusChange', 'Nerman.ThreadStatusChange');
+events.set('threadVote', 'Nerman.ThreadVote');
 events.set('auctionBid', 'NounsDAO.AuctionBid');
 events.set('auctionCreated', 'NounsDAO.AuctionCreated');
 events.set('delegateChanged', 'NounsDAO.DelegateChanged');
 events.set('delegateChangedNoZero', 'NounsDAO.DelegateChangedNoZero');
-events.set('newPost', 'NounsNymz.NewPost');
-events.set('newProposalPoll', 'Nerman.NewProposalPoll');
 events.set('nounCreated', 'NounsDAO.NounCreated');
 events.set('propCreated', 'NounsDAO.PropCreated');
 events.set('propStatusChange', 'NounsDAO.PropStatusChange');
 events.set('propVoteCast', 'NounsDAO.PropVoteCast');
-events.set('threadVote', 'Nerman.ThreadVote');
-events.set('threadStatusChange', 'Nerman.ThreadStatusChange');
 events.set('transferNoun', 'NounsDAO.TransferNoun');
+events.set('newPost', 'NounsNymz.NewPost');
 module.exports = events;
