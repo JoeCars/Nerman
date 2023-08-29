55ccdfa0329916671d8cb3a909e0573b24927ec5
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-08-29 13:05:26 -0700

    Implemented signature added handler.

---
+++ views/embeds/signatureAdded.js
@@ -0,0 +1,42 @@
+const { MessageEmbed } = require('discord.js');
+const { hyperlink } = require('@discordjs/builders');
+
+/**
+ * @param {{slug: string, proposer: {id: string, name: string}, signer: {id: string, name: string}, reason: string}} data
+ */
+exports.generateProposalCandidateCreatedEmbed = function (data) {
+   let proposalTitle = data.slug
+      .split('-')
+      .filter(word => {
+         return word.trim();
+      })
+      .map(word => {
+         return word[0].toUpperCase() + word.substring(1);
+      })
+      .join(' ');
+   if (proposalTitle.length > 150) {
+      proposalTitle = proposalTitle.substring(0, 150) + '...';
+   }
+   const title = `Candidate Proposal Signed: ${proposalTitle}`;
+
+   const proposer = hyperlink(
+      data.proposer.name,
+      `https://etherscan.io/address/${data.proposer.id}`,
+   );
+   const signer = hyperlink(
+      data.signer.name,
+      `https://etherscan.io/address/${data.signer.id}`,
+   );
+   const reason = data.reason ? `\n\n${data.reason}` : '';
+   const description = `${proposer} signed ${signer}'s candidate proposal.${reason}`;
+
+   const url = `https://nouns.wtf/candidates/${data.proposer.id}-${data.slug}`;
+
+   const embed = new MessageEmbed()
+      .setColor('#00FFFF')
+      .setTitle(title)
+      .setDescription(description)
+      .setURL(url);
+
+   return embed;
+};
