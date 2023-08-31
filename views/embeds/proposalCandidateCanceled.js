b31804b8b24cdcdfda0ecb9a20c910f8cf05c22a
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-08-31 08:12:14 -0700

    Created canceled embed.

---
+++ views/embeds/proposalCandidateCanceled.js
@@ -0,0 +1,38 @@
+const { MessageEmbed } = require('discord.js');
+const { hyperlink, inlineCode } = require('@discordjs/builders');
+
+/**
+ * @param {{slug: string,
+ * msgSender: {id: string, name: string},
+ * reason: string}} proposal
+ */
+exports.generateProposalCandidateCanceledEmbed = function (proposal) {
+   const title = `Proposal Candidate Canceled`;
+
+   const proposer = hyperlink(
+      proposal.msgSender.name,
+      `https://etherscan.io/address/${proposal.msgSender.id}`,
+   );
+   const proposalTitle =
+      proposal.slug
+         .trim()
+         .split('-', 3)
+         .filter(word => {
+            return word.trim();
+         })
+         .map(word => {
+            return word[0].toUpperCase() + word.substring(1).toLowerCase();
+         }) + +'...';
+   const proposalUrl = `https://nouns.wtf/candidates/${proposal.msgSender.id}-${proposal.slug}`;
+   const proposalName = hyperlink(proposalTitle, proposalUrl);
+   const description = `${proposer} has ${inlineCode(
+      'CANCELED',
+   )} their proposal candidate (${proposalName}).`;
+
+   const embed = new MessageEmbed()
+      .setColor('#00FFFF')
+      .setTitle(title)
+      .setDescription(description);
+
+   return embed;
+};
