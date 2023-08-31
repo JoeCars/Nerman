fbaef272636dbf254e2281939318acc919c9e427
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-08-31 11:28:25 -0700

    Moved nerman admin commands.

--- commands/slashCommands/nerman.js
+++ commands/slashCommands/nerman.js
@@ -204,42 +204,6 @@ module.exports = {
                   });
             });
       })
-      .addSubcommandGroup(subcommandGroup => {
-         return subcommandGroup
-            .setName('admin')
-            .setDescription(
-               'Commands to add, remove, and display the guild admins.',
-            )
-            .addSubcommand(subcommand => {
-               return subcommand
-                  .setName('add')
-                  .setDescription('Give a user admin privileges for Nerman.')
-                  .addUserOption(option => {
-                     return option
-                        .setName('user')
-                        .setDescription('The user being turned into an admin.')
-                        .setRequired(true);
-                  });
-            })
-            .addSubcommand(subcommand => {
-               return subcommand
-                  .setName('remove')
-                  .setDescription(
-                     "Removing a user' admin privileges for Nerman.",
-                  )
-                  .addUserOption(option => {
-                     return option
-                        .setName('user')
-                        .setDescription('The user being removed from an admin.')
-                        .setRequired(true);
-                  });
-            })
-            .addSubcommand(subcommand => {
-               return subcommand
-                  .setName('display')
-                  .setDescription('Displaying the admin users in this guild.');
-            });
-      })
       .addSubcommand(subcommand => {
          return subcommand
             .setName('register-nouns-forum')
