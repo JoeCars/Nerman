6cc63ac003ca24ef379f76e45d09da6df9bbe3e0
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-09-05 15:17:22 -0700

    Created event handler.

---
+++ events/customEvents/poll/threadFederationVoteCast.js
@@ -0,0 +1,46 @@
+const { TextChannel } = require('discord.js');
+
+const Logger = require('../../../helpers/logger');
+const { findPollMessage } = require('../../../helpers/poll/thread');
+const {
+   generateFederationVoteEmbed,
+} = require('../../../views/embeds/federation/voteCast');
+const UrlConfig = require('../../../db/schemas/UrlConfig');
+
+module.exports = {
+   name: 'threadFederationVoteCast',
+   /**
+    * @param {TextChannel} channel
+    */
+   async execute(channel, data) {
+      try {
+         const pollMessage = await findPollMessage(channel, data.proposalId);
+         if (!pollMessage) {
+            return;
+         }
+         const propUrl = (await UrlConfig.fetchUrls(channel.guildId)).propUrl;
+         const threadEmbed = generateFederationVoteEmbed(data, propUrl);
+         await pollMessage.thread.send({
+            content: null,
+            embeds: [threadEmbed],
+         });
+      } catch (error) {
+         return Logger.error(
+            'events/poll/threadFederationVoteCast.js: Received error.',
+            {
+               error: error,
+               guildId: channel.guildId,
+               channelId: channel.id,
+            },
+         );
+      }
+
+      Logger.info(
+         'events/poll/threadFederationVoteCast.js: Finished sending threadFederationVoteCast.',
+         {
+            guildId: channel.guildId,
+            channelId: channel.id,
+         },
+      );
+   },
+};
