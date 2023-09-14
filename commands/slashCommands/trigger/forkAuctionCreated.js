975d3d7db22ff507af43a113d606305536250ced
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-09-14 15:04:20 -0700

    Created auctionCreated trigger.

---
+++ commands/slashCommands/trigger/forkAuctionCreated.js
@@ -0,0 +1,49 @@
+const { CommandInteraction } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const Logger = require('../../../helpers/logger');
+const { authorizeInteraction } = require('../../../helpers/authorization');
+
+const DEFAULT_NOUN_ID = 117;
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName('trigger-fork-auction-created')
+      .setDescription('Trigger an auction created event.')
+      .addNumberOption(option => {
+         return option
+            .setName('noun-number')
+            .setDescription('The noun being auctioned.')
+            .setRequired(process.env.DEPLOY_STAGE !== 'development');
+      }),
+
+   /**
+    * @param {CommandInteraction} interaction
+    */
+   async execute(interaction) {
+      await authorizeInteraction(interaction, 4);
+
+      const nounId =
+         interaction.options.getNumber('noun-number') ?? DEFAULT_NOUN_ID;
+
+      const nounsForkAuctionHouse = interaction.client.libraries.get(
+         'NounsForkAuctionHouse',
+      );
+      nounsForkAuctionHouse.emit('AuctionCreated', {
+         id: nounId,
+      });
+
+      interaction.reply({
+         ephemeral: true,
+         content: 'Triggered a ForkAuctionCreated event.',
+      });
+
+      Logger.info(
+         'commands/trigger/forkAuctionCreated.js: A forkAuctionCreated event has been triggered.',
+         {
+            guildId: interaction.guildId,
+            channelId: interaction.channelId,
+            userId: interaction.user.id,
+         },
+      );
+   },
+};
