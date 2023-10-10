2f4eb8c748a3c2423e2ec3d7487df9458f7b303e
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-10-10 11:38:02 +0400

    Added transaction url to embed.

--- views/embeds/forkProposalCreated.js
+++ views/embeds/forkProposalCreated.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require('discord.js');
 
 exports.generateForkProposalCreatedEmbed = function (proposal) {
    const title = 'Fork 0 | New Proposal';
+   const titleUrl = `https://etherscan.io/tx/${data.event.transactionHash}`;
    let proposalName = `Proposal ${proposal.id}`;
    let proposalSubtitle = proposal.description.match(
       /^(#\s(?:\S+\s?)+(?:\S+\n?))/,
@@ -14,7 +15,8 @@ exports.generateForkProposalCreatedEmbed = function (proposal) {
    const proposalEmbed = new MessageEmbed()
       .setColor('#00FFFF')
       .setTitle(title)
-      .setDescription(`\n${proposalName}`);
+      .setDescription(`\n${proposalName}`)
+      .setURL(titleUrl);
 
    return proposalEmbed;
 };
