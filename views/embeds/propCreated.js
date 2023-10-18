db4c92d61845ee430630af98bcfc870573400b51
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-10-18 14:32:33 +0400

    Fixed PropCreated embed bug.
    The regex was causing issues because it assumed only 1 space after '#'.
    But prop 413 had 2 spaces.

--- views/embeds/propCreated.js
+++ views/embeds/propCreated.js
@@ -3,9 +3,7 @@ const { hideLinkEmbed } = require('@discordjs/builders');
 
 exports.generatePropCreatedEmbed = function (proposal, proposalUrl) {
    const title = 'New Proposal!';
-   const proposalName = `Proposal ${proposal.id}: ${proposal.description
-      .match(/^(#\s(?:\S+\s?)+(?:\S+\n?))/)[0]
-      .replaceAll(/^(#\s)|(\n+)$/g, '')}`;
+   const proposalName = proposal.proposalTitle;
    const url = `${proposalUrl}${proposal.id}`;
 
    const proposalEmbed = new MessageEmbed()
