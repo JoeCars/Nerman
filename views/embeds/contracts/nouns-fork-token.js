87bd85cd9b3d041614881517492a7dac2f7f1303
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2023-11-29 13:34:18 -0800

    Added Noun Created embed for forks.

--- views/embeds/contracts/nouns-fork-token.js
+++ views/embeds/contracts/nouns-fork-token.js
@@ -75,3 +75,20 @@ exports.generateForkDelegateChangedEmbed = function (data, hasMarkdown = true) {
 
    return embed;
 };
+
+exports.generateForkNounCreatedEmbed = function (data) {
+   const title = `Fork 0 | Noun Created | Noun ${data.id}`;
+
+   const titleUrl = `https://nouns.wtf/noun/${data.id}`;
+
+   const embed = new MessageEmbed()
+      .setColor('#00FFFF')
+      .setTitle(title)
+      .setURL(titleUrl)
+      .setDescription(
+         `${data.id % 10 !== 0 ? 'Auction Created' : "Nounder's Noun"}`,
+      )
+      .setImage(`http://noun.pics/${data.id}.png`);
+
+   return embed;
+};
