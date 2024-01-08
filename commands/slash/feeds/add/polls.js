87906a67507545c2eab76836d2104d2c98cb40c3
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-01-08 11:48:20 -0800

    Added poll guard.

--- commands/slash/feeds/add/polls.js
+++ commands/slash/feeds/add/polls.js
@@ -1,10 +1,27 @@
+const { CommandInteraction } = require('discord.js');
 const add = require('./add');
+const PollChannel = require('../../../../db/schemas/PollChannel');
+
 module.exports = {
    subCommand: 'nerman-feeds.add.polls',
    /**
     * @param {CommandInteraction} interaction
     */
    async execute(interaction) {
+      const channel =
+         interaction.options.getChannel('channel') ?? interaction.channel;
+      const numOfChannels = await PollChannel.countDocuments({
+         channelId: channel.id,
+      }).exec();
+
+      if (numOfChannels === 0) {
+         return interaction.reply({
+            content:
+               'This feed can only be added to poll channels. Please create a poll channel first.',
+            ephemeral: true,
+         });
+      }
+
       add.execute(interaction);
    },
 };
