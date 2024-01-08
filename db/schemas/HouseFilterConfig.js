e29fb10044b926cb48c8e7b07892c57065a52054
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-01-08 14:54:35 -0800

    Implemented schema.

---
+++ db/schemas/HouseFilterConfig.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const HouseFilterConfigSchema = new Schema(
+   {
+      _id: Schema.Types.ObjectId,
+      guildId: {
+         type: String,
+         required: true,
+      },
+      channelId: {
+         type: String,
+         required: true,
+      },
+      filteredHouses: [
+         {
+            type: [String],
+            required: true,
+         },
+      ],
+      isDeleted: {
+         type: Boolean,
+         required: false,
+         default: false,
+      },
+   },
+   {
+      methods: {
+         async softDelete() {
+            this.isDeleted = true;
+            return this.save();
+         },
+      },
+   },
+);
+
+module.exports = model('HouseFilterConfig', HouseFilterConfigSchema);
