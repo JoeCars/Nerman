42f46731f2eba41f8a91e6944ed9252224925ccc
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2025-08-13 18:08:57 -0700

    feat(Polling Interval): add new POLLING_INTERVAL_MS .env var for nerman

--- utils/nouns/importNerman.js
+++ utils/nouns/importNerman.js
@@ -4,6 +4,6 @@ module.exports = async function importNermanJS() {
    const _nerman = import('nerman');
    const nerman = await _nerman;
    return new nerman.Nouns(process.env.JSON_RPC_API_URL, {
-      pollingTime: DEFAULT_POLLING_TIME,
+      pollingTime: Number(process.env.POLLING_INTERVAL_MS) ?? DEFAULT_POLLING_TIME,
    });
 };
